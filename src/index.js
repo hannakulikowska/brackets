4becd34e376d46d41a1118680e8cf511f40d70e0
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2023-10-27 13:56:10 +0200

    feat: add function to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newStr = str;
+  
+  for (let pair of bracketsConfig) {
+    let brackets = pair.join("");
+    while (newStr.includes(brackets)) {
+      newStr = newStr.replace(brackets, "");
+      console.log(newStr);
+    }
+  }
+
+  if (newStr.length === str.length) {
+    return false;
+  }
+  if (newStr === "") {
+    return true;
+  }
+
+  return check(newStr, bracketsConfig);
 }
